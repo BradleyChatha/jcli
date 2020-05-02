b44b641de74f8ded6caeb909cfbe85733378a774
SealabJaster <sealabjaster@gmail.com>
2020-05-02 15:40:19 +0100

    Change missing args from using enforce, to properly using executionError

--- source/jaster/cli/core.d
+++ source/jaster/cli/core.d
@@ -527,16 +527,18 @@ final class CommandLineInterface(Modules...)
                 // Check for missing args.
                 auto missingNamedArgs      = namedArgs.filter!(a => !a.isNullable && !a.wasFound);
                 auto missingPositionalArgs = positionalArgs.filter!(a => !a.isNullable && !a.wasFound);
-                enforce(
-                    missingNamedArgs.empty, 
-                    "The following required named arguments were not provided: %s"
-                    .format(missingNamedArgs.map!(a => a.uda.pattern))
-                );
-                enforce(
-                    missingPositionalArgs.empty, 
-                    "The following required positional arguments were not provided: %s"
-                    .format(missingPositionalArgs.map!(a => format("[%s] %s", a.uda.position, a.uda.name)))
-                );
+                if(!missingNamedArgs.empty)
+                {
+                    executionError = "The following required named arguments were not provided: %s"
+                                     .format(missingNamedArgs.map!(a => a.uda.pattern));
+                    return -1;
+                }
+                if(!missingPositionalArgs.empty)
+                {
+                    executionError = "The following required positional arguments were not provided: %s"
+                                      .format(missingPositionalArgs.map!(a => format("[%s] %s", a.uda.position, a.uda.name)));
+                    return -1;
+                }
 
                 // Execute the command.
                 static assert(__traits(compiles, commandInstance.onExecute()),
