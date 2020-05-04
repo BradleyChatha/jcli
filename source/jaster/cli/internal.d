ffe772880cda851ecf40c7734a948fb1fc4f1066
SealabJaster <sealabjaster@gmail.com>
2020-05-04 20:13:47 +0100

    Docs for internal.d

--- source/jaster/cli/internal.d
+++ source/jaster/cli/internal.d
@@ -1,5 +1,7 @@
+/// Internal utilities.
 module jaster.cli.internal;
 
+/// pragma(msg) only used in debug mode, if version JCLI_Verbose is specified.
 void debugPragma(string Message)()
 {
     version(JCLI_Verbose)
