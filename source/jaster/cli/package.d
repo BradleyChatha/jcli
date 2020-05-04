88000394ebe92861f6f1b59825749135115d9d74
SealabJaster <sealabjaster@gmail.com>
2020-05-04 19:14:21 +0100

    Add UserIO into package.d

--- source/jaster/cli/package.d
+++ source/jaster/cli/package.d
@@ -1,4 +1,4 @@
 module jaster.cli;
 
 public import jaster.cli.binder, jaster.cli.core, jaster.cli.helptext, jaster.cli.parser,
-              jaster.cli.udas, jaster.cli.shell, jaster.cli.ansi;
+              jaster.cli.udas, jaster.cli.shell, jaster.cli.ansi, jaster.cli.userio;
