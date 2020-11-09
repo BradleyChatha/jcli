a433769ba3717144f718ebd68a53ef42ed4f921c
Andrey Zherikov <andrey.zherikov@gmail.com>
2020-11-09 14:31:50 -0500

    Nullable _defaultCommand

--- source/jaster/cli/core.d
+++ source/jaster/cli/core.d
@@ -751,7 +751,7 @@ final class CommandLineInterface(Modules...)
     {
         CommandResolver!CommandInfo _resolver;
         ServiceProvider             _services;
-        CommandInfo                 _defaultCommand;
+        Nullable!CommandInfo        _defaultCommand;
         string                      _appName;
     }
 
@@ -826,7 +826,7 @@ final class CommandLineInterface(Modules...)
             import std.stdio     : writefln;
             import std.format    : format;
 
-            if(args.empty && this._defaultCommand == CommandInfo.init)
+            if(args.empty && _defaultCommand.isNull)
             {
                 writefln(this.makeErrorf("No command was given."));
                 writefln(this.createAvailableCommandsHelpText(args, "Available commands").toString());
@@ -851,20 +851,20 @@ final class CommandLineInterface(Modules...)
                 if(args.containsHelpArgument())
                 {
                     parseResult.type = ParseResultType.showHelpText;
-                    if(this._defaultCommand != CommandInfo.init)
-                        parseResult.helpText ~= this._defaultCommand.helpText.toString();
+                    if(!_defaultCommand.isNull)
+                        parseResult.helpText ~= _defaultCommand.get.helpText.toString();
                     
                     if(this._resolver.finalWords.length > 0)
                         parseResult.helpText ~= this.createAvailableCommandsHelpText(parseResult.argParserBeforeAttempt, "Available commands").toString();
                 }
-                else if(this._defaultCommand == CommandInfo.init)
+                else if(_defaultCommand.isNull)
                 {
                     parseResult.type      = ParseResultType.commandNotFound;
                     parseResult.helpText ~= this.makeErrorf("Unknown command '%s'.\n", parseResult.argParserBeforeAttempt.front.value);
                     parseResult.helpText ~= this.createAvailableCommandsHelpText(parseResult.argParserBeforeAttempt).toString();
                 }
                 else
-                    parseResult.command = this._defaultCommand;
+                    parseResult.command = _defaultCommand.get;
             }
             else
                 parseResult.command = result.value.userData;
