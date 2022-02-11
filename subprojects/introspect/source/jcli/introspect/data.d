6555b2060e4337bd227cc54024cd211e65ebc664
AntonC9018 <anton.curm@gmail.com>
2022-02-11 11:16:39 +0200

    ideas

--- subprojects/introspect/source/jcli/introspect/data.d
+++ subprojects/introspect/source/jcli/introspect/data.d
@@ -2,6 +2,35 @@ module jcli.introspect.data;
 
 import jcli.core;
 
+
+private
+{
+    /// Shows which other flags a feature is incompatible with.
+    /// The flags should only declare this regarding the flags after them. 
+    struct IncompatibleWithAnyOf
+    {
+        ArgFlags value;
+    }
+
+    /// Shows which other flags a feature is requred to be accompanied with.
+    struct RequiresOneOrMoreOf
+    {
+        ArgFlags value;
+    }
+
+    /// Shows which flags a feature requres exactly one of.
+    struct RequiresExactlyOneOf
+    {
+        ArgFlags value;
+    }
+
+    /// Shows which other flags a feature is requres with.
+    struct RequiresAllOf
+    {
+        ArgFlags value;
+    }
+}
+
 enum ArgFlags
 {
     ///
@@ -11,52 +40,164 @@ enum ArgFlags
     _optionalBit = 1 << 0,
 
     /// An argument with the same name may appear multiple times.
+    @RequiresExactlyOneOf(countBit | canRedefineBit | aggregateBit)
     _multipleBit = 1 << 1,
 
     ///
+    @IncompatibleWithAnyOf(_multipleBit)
+    @RequiresAllOf(_optionalBit)
     _parseAsFlagBit = 1 << 2,
 
     /// Implies that the field should get the number of occurences of the argument.
+    @RequiresOneOrMoreOf(_mutipleBit | _repeatableNameBit)
     _countBit = 1 << 3,
 
     /// The name of the argument is case insensitive.
     /// Aka "--STUFF" will work in place of "--stuff".
     _caseInsensitiveBit = 1 << 4,
-    
+
     /// If the argument appears multiple times, 
     /// the last value provided will take effect.
+    @IncompatibleWithAnyOf(_countBit)
+    @RequiresAllOf(_multipleBit)
     _canRedefineBit = 1 << 5,
 
     /// When an argument name is specified multiple times, count how many there are.
     /// Example: `-vvv` gives the count of 3.
+    @IncompatibleWithAnyOf(_parseAsFlagBit | _canRedefineBit)
+    @RequiresAllOf(_countBit)
     _repeatableNameBit = 1 << 6,
 
     /// Put all matched values in an array. 
+    @IncompatibleWithAnyOf(_parseAsFlagBit | _countBit | _canRedefineBit)
     _aggregateBit = 1 << 7,
+}
+
+private
+{
+    enum argFlagCount =
+    (){
+        size_t counter = 0;
+        static foreach (field; __traits(allMembers, ArgFlags))
+        {
+            if (__traits(getMember, ArgFlags, field) > 0)
+            {
+                counter++;
+            }
+        }
+        return counter;
+    }();
+
+
+    ArgFlags[argFlagCount] getFlagsInfo(TUDA)()
+    {
+        ArgFlags[argFlagCount] result;
+
+        import std.bitmanip : bitsSet; 
+        static foreach (field; __traits(allMembers, ArgFlags))
+        {{
+            size_t index = __traits(getMember, ArgFlags, field).bitsSet.front;
+            static foreach (uda; __traits(getAttributes, field))
+            {
+                static if (is(typeof(uda) == TUDA))
+                {
+                    result[index] = uda.value;
+                }
+            }
+        }}
+
+        return result;
+    }
+
+    template _ArgFlagsInfo()
+    {
+        immutable ArgFlags[argFlagCount] incompatible        = getFlagsInfo!IncompatibleWithAnyOf;
+        immutable ArgFlags[argFlagCount] requiresAll         = getFlagsInfo!RequiresAllOf;
+        immutable ArgFlags[argFlagCount] requiresExactlyOne  = getFlagsInfo!RequiresExactlyOneOf;
+        immutable ArgFlags[argFlagCount] requiresOneOrMore   = getFlagsInfo!RequiresOneOrMoreOf;
+    }
+    alias ArgFlagsInfo = _ArgFlagsInfo!();
+
+
+    string getArgumentFlagsValidationMessage(ArgFlags flags)
+    {
+        import std.bitmanip : bitsSet;        
+        import std.conv : to;
+
+        foreach (size_t index; bitsSet(flags))
+        {
+            const currentFlag = 2 ^^ index;
+            {
+                const f = ArgFlagsInfo.incompatible[index];
+                if (flags.hasEither(f))
+                {
+                    return "The flag " ~ currentFlag.to!string ~ " is incompatible with " ~ f.to!string;
+                }
+            }
+            {
+                const f = ArgFlagsInfo.requiresAll[index];
+                if (flags.doesNotHave(f))
+                {
+                    return "The flag " ~ currentFlag.to!string ~ " requires all of " ~ f.to!string;
+                }
+            }
+            {
+                const f = ArgFlagsInfo.requiresOneOrMore[index];
+                if (f != 0 && flags.doesNotHaveEither(f))
+                {
+                    return "The flag " ~ currentFlag.to!string ~ " requires one or more of " ~ f.to!string;
+                }
+            }
+            {
+                const f = ArgFlagsInfo.requiresExactlyOne[index];
+                if (f != 0)
+                {
+                    auto matches = currentFlag & f;
+
+                    import std.range : walkLength;
+                    auto numMatches = bitsSet(matches).walkLength;
+
+                    if (numMatches != 1)
+                    {
+                        return "The flag " ~ currentFlag.to!string ~ " requires exactly one of " ~ f.to!string
+                            ~ ". Were matched all of the following: " ~ matches.to!string;
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+}
+
+enum ArgConfig : ArgFlags
+{
+    ///
+    none,
 
     ///
-    canRedefine = _canRedefineBit | _multipleBit,
+    canRedefine = ArgFlags._canRedefineBit | ArgFlags._multipleBit,
 
     /// If not given a value, will have its default value and not trigger an error.
     /// Missing (even named) arguments trigger an error by default.
-    optional = _optionalBit,
+    optional = ArgFlags._optionalBit,
 
     ///
-    caseInsesitive = _caseInsensitiveBit,
+    caseInsesitive = ArgFlags._caseInsensitiveBit,
 
     /// Example: `-a -a` gives 2.
-    accumulate = _multipleBit | _countBit,
+    accumulate = ArgFlags._multipleBit | ArgFlags._countBit,
 
     /// The type of the field must be an array of some sort.
     /// Example: `-a b -a c` gives an the array ["b", "c"]
-    aggregate = _multipleBit | _aggregateBit,
+    aggregate = ArgFlags._multipleBit | ArgFlags._aggregateBit,
 
     ///
-    repeatableName = _repeatableNameBit | _countBit,
+    repeatableName = ArgFlags._repeatableNameBit | ArgFlags._countBit,
 
     /// Allow an argument name to appear without a value.
     /// Example: `--flag` would parse as `true`.
-    parseAsFlag = _parseAsFlagBit | _optionalBit,
+    parseAsFlag = ArgFlags._parseAsFlagBit | ArgFlags._optionalBit,
 }
 
 package (jcli)
@@ -72,6 +213,10 @@ package (jcli)
     bool hasEither(ArgFlags a, ArgFlags b)
     {
         return (a & b) != 0;
+    }
+    bool doesNotHaveEither(ArgFlags a, ArgFlags b)
+    {
+        return (a & b) == 0;
     }
 }
 
@@ -103,12 +248,17 @@ struct PositionalArgumentInfo
 
 template CommandInfo(TCommand)
 {
-    static foreach (field; TCommand.tupleof)
-        static assert(countUDAsOf!(field, ArgNamed, ArgPositional, ArgOverflow, ArgRaw).length <= 1);
-
     alias CommandT = TCommand;
+    immutable CommandInfo general = getCommandGeneralInfoOf!TCommand;
+    alias arguments = CommandArgumentsInfo!TCommand;
+}
 
-    immutable CommandGeneralInfo       general          = getCommandGeneralInfoOf!TCommand;
+
+template CommandArgumentsInfo(TCommand)
+{
+    static foreach (field; TCommand.tupleof)
+        static assert(countUDAsOf!(field, string, ArgNamed, ArgPositional, ArgOverflow, ArgRaw).length <= 1);
+
     immutable NamedArgumentInfo[]      namedArgs        = [ argumentInfosOf!ArgNamed ];
     immutable PositionalArgumentInfo[] positionalArgs   = [ argumentInfosOf!PositionalArgumentInfo ];
 
@@ -119,40 +269,6 @@ template CommandInfo(TCommand)
     enum takesRawArgs = is(typeof(fieldWithUDAOf!ArgRaw));
     static if (takesRawArgs)
         immutable ArgumentCommonInfo rawArg = getCommonArgumentInfo!(fieldWithUDAOf!ArgRaw);
-}
-
-/// TODO: maybe??
-string getArgumentFlagsValidationMessage(ArgFlags flags)
-{
-    string[] ret;
-    with (ArgFlags)
-    {
-        immutable leftAlwaysPairsWithRight = [
-            [_parseAsFlagBit, _optionalBit],
-            [_canRedefineBit, _multipleBit],
-            [_repeatableNameBit, _countBit],
-            [_aggregateBit, _multipleBit],
-        ];
-
-        // immutable cannotGoWithEither = [
-        //     [_multipleBit, _parseAsFlagBit], // probably
-        //     [_countBit, _aggregateBit, 
-        //     _caseInsensitiveBit
-        //     _canRedefineBit
-        //     _repeatableNameBit
-        //     _aggregateBit
-
-        //     [_countBit, _parseAsFlagBit, _canRedefineBit, _aggregateBit],
-        //     [_aggregateBit, _parseAsFlagBit, _repeatableNameBit],
-        //     [_canRedefineBit, 
-        // ];
-
-        if (flags.has(aggregate) && flags.has(parseAsFlag))
-        {
-            ret ~= "`aggregate` cannot be used together with `parseAsFlag`";
-        }
-    }
-    return null;
 }
 
 
@@ -198,13 +314,13 @@ ArgumentCommonInfo getCommonArgumentInfo(T, alias field)() pure
 {
     import std.conv : to;
     ArgumentCommonInfo result;
-    ArgFlags[] recordedFlags = [];
+    // ArgFlags[] recordedFlags = [];
     
     static foreach (uda; __traits(getAttributes, field))
     {
         static if (is(typeof(uda) == ArgFlags))
         {
-            recordedFlags ~= uda;
+            // recordedFlags ~= uda;
             result.flags |= uda;
         }
         else static if (is(typeof(uda) == ArgGroup))
@@ -214,15 +330,10 @@ ArgumentCommonInfo getCommonArgumentInfo(T, alias field)() pure
         }
     }
 
-    foreach (flag0; recordedFlags)
-    foreach (flag1; recordedFlags[1 .. $])
     {
-        assert(isFlagCombinationSupported(flag0, flag1),
-            "The flag combination " ~ flag0.to!string ~ " and " ~ flag1.to!string ~ "is not supported");
+        auto validationMessage = getArgumentFlagsValidationMessage(result.flags);
+        assert(validationMessage is null, validationMessage);
     }
-
-    assert(isFlagCombinationSupported(result.flags, result.flags),
-        "Bad flag combination: " ~ result.flags.to!string);
 
     static if (is(typeof(group)))
         result.group = group;
@@ -254,6 +365,27 @@ UDAType getArgumentInfo(UDAType, alias field)() pure
     }
     static assert(is(typeof(uda1)), "`" ~ UDAType.stringof ~ "` attribute not found.");
     result.uda = uda1;
+    return result;
+}
+
+ArgNamed getSimpleArgumentInfo(alias field)() pure
+{
+    ArgNamed result;
+    result.argument = getCommonArgumentInfo!(ArgNamed, field)();
+
+    string description =
+    (){
+        static foreach (uda; __traits(getAttributes, field))
+        {
+            static if (is(typeof(uda) == string))
+            {
+                return uda;
+            }
+        }
+        static assert(0);
+    }();
+
+    result.uda = ArgNamed(__traits(identifier, field), description);
     return result;
 }
 
@@ -352,16 +484,19 @@ template UDAInfo(alias uda)
 
 template countUDAsOf(alias something, UDATypes...)
 {
-    import std.meta : Alias;
-    alias countUDAsOf = Alias!(0);
-    static foreach (UDAType; UDATypes)
-    {
-        static foreach (uda; __traits(getAttributes, something))
+    enum countUDAsOf =
+    (){
+        size_t result = 0;
+        static foreach (UDAType; UDATypes)
         {
-            static if (is(UDAInfo!uda.Type == UDAType))
-                countUDAsOf = Alias!(countUDAsOf + 1);
+            static foreach (uda; __traits(getAttributes, something))
+            {
+                static if (is(UDAInfo!uda.Type == UDAType))
+                    result++;
+            }
         }
-    }
+        return result;
+    }();
 }
 
 template hasExactlyOneOfUDAs(alias something, UDATypes...)
